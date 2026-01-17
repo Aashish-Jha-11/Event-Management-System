8580243bb83ecda2ffc3fc1fc9d8dcc310aa4da3
Aashish-Jha-11 <aashishjha1107@gmail.com>
2026-01-17 19:22:53 +0530

    fix preflight options handling

--- backend/server.js
+++ backend/server.js
@@ -10,19 +10,16 @@ connectDB();
 
 const app = express();
 
-const corsOptions = {
-  origin: [
-    'https://event-management-system-bv1y.vercel.app',
-    'http://localhost:3000'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  optionsSuccessStatus: 200
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
